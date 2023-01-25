e6ef2c547827a38b300fc7eea874e289eb4c76cc
RKK <raghu.chn2@gmail.com>
2023-01-25 11:01:29 +0530

    My first project - ERIC

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "myregion" {
+type = string
+description = "AWS region to use"
+}
+
+variable "myaccesskey" {
+description = "AWS access key"
+}
+variable "mysecretkey" {
+description = "AWS secret key"
+}
